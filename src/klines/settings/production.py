036731e76ccbefdb5ed75fad73763fbeb7c8214b
Ilnur-Sultanov <isultanov99@gmail.com>
2021-12-17 14:04:54 +0300

    static

--- src/klines/settings/production.py
+++ src/klines/settings/production.py
@@ -132,10 +132,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static/klines"]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
