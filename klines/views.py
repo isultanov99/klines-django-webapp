2f142698ef5309cc262e9df3944805d36992e2ba
Ilnur-Sultanov <isultanov99@gmail.com>
2021-12-17 16:22:49 +0300

    fixing templates

--- klines/views.py
+++ klines/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import redirect
 
 
 def page_not_found_view(request, exception):
-    return render(request, '../templates/404.html', status=404)
+    return render(request, '../templates/404', status=404)
 
 
 def check_input(get_request):
@@ -22,11 +22,11 @@ def home(request):
     if request.method == "POST" and check_input(request.POST):
         return redirect('route/' + Station.objects.get(name=str(request.POST.get('from_field'))).slug + '/' +
                         Station.objects.get(name=str(request.POST.get('to_field'))).slug)
-    return render(request, '../templates/home.html')
+    return render(request, '../templates/home')
 
 
 def route(request, first_param=None, second_param=None):
     context = {"first_param": Station.objects.get(slug=first_param),
                "second_param": Station.objects.get(slug=second_param),
                "trains": Train.objects.all(), "weekday": ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]}
-    return render(request, '../templates/route.html', context)
+    return render(request, '../templates/route', context)
