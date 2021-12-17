5545abbb27e3489e72ed47c4d0285dc51432bd60
Ilnur-Sultanov <isultanov99@gmail.com>
2021-12-17 15:35:12 +0300

    Facepalm. Wrong project structure

--- trains/views.py
+++ trains/views.py
@@ -8,11 +8,11 @@ def trains_list(request):
     sl = Station.objects.all()
     wd = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
     context = {'trains_list': tl, 'stations_list': sl, 'weekday': wd}
-    return render(request, 'trains/list.html', context)
+    return render(request, 'trains/../templates/trains/list.html', context)
 
 
 def detail(request, num):
     qs = get_object_or_404(Train, num=num)
     wd = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"]
     context = {'object': qs, 'station': Station.objects.all(), 'weekday': wd}
-    return render(request, 'trains/detail.html', context)
+    return render(request, 'trains/../templates/trains/detail.html', context)
